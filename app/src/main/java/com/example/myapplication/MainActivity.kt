4c4eb6b132ce8556b4ac9fc7e367449b83a1166a
osamax2009 <osamafalah85@yahoo.com>
2024-03-03 20:22:32 +0300

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,4 @@
 package  com.example.myapplication
-import com.example.myapplication.R
 
 
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.data.local.entity.Todo
 import com.example.myapplication.ui.TodoAdappter
 import com.example.myapplication.ui.TodoViewModel
+import com.scienceexperts.mayartodos.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
